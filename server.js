b4b92660399bcdfb451f705fafab08a5e74b8434
grokljay-lang <grokljay@gmail.com>
2026-04-26 15:55:14 +0300

    fix server js

--- server.js
+++ server.js
@@ -120,3 +120,8 @@ app.post("/recovery", async (req, res) => {
 app.listen(3000, () => {
   console.log("Сервер: http://localhost:3000");
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Сервер: http://localhost:${PORT}`);
+});
